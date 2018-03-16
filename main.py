bcf439496de07fce2e196e89ce28af3b56356d49
Len Lanphar <lanphar@gmail.com>
2018-03-16 08:59:34 -0400

    added pot control for speed and brightness

--- main.py
+++ main.py
@@ -21,7 +21,12 @@ strip = neopixel.NeoPixel(pixpin, numpix, brightness=1.0, auto_write=False)
 
 pot = analogio.AnalogIn(board.A0)
 
+brightness = 255
+
 hues=[234,200,156]
+rgb_colors=[]
+for i in range(len(hues)):
+    rgb_colors.append([0,0,0])
 
 min_color_length=10
 max_color_length=25
@@ -35,19 +40,27 @@ def shuffle(list):
     tmp=list[swap]
     list[swap]=list[0]
     list[0]=tmp
+
+# calculate the rgb values for each hue at the current brightness
+def calculate_rgb(hues,rgb_colors,brightness):
+    for i in range(len(hues)):
+        hue=hues[i]
+        rgb=hsv2rgb((hue,255,brightness))
+        rgb_colors[i][0]=rgb[0]
+        rgb_colors[i][1]=rgb[1]
+        rgb_colors[i][2]=rgb[2]
     
-def new_strip_hues(led_colors):
+def new_strip_colors(rgb_colors,led_colors):
     global hues
     idx=0
-    shuffle(hues)
-    hue_idx=0
+    shuffle(rgb_colors)
+    color_idx=0
     while idx < numpix:
-        hue=hues[hue_idx]
-        hue_idx+=1
-        if hue_idx >= len(hues):
-            hue_idx=0
+        rgb=rgb_colors[color_idx]
+        color_idx+=1
+        if color_idx >= len(rgb_colors):
+            color_idx=0
         length=randint(min_color_length,max_color_length)
-        rgb=hsv2rgb((hue,255,255))
         for i in range(length):
             if idx >= numpix:
                 break
@@ -56,18 +69,46 @@ def new_strip_hues(led_colors):
             led_colors[idx][2]=rgb[2]
             idx+=1
 
-new_strip_hues(led_colors)
+def map(val,in_min,in_max,out_min,out_max):
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+min_sleep=0.03
+max_sleep=0.2
+
+# knock down the precision of the read value to alleviate jitter
+last_pot_val=pot.value>>12
+brightness=int(map(last_pot_val,0,15,128,255))
+sleep_time=map(15-last_pot_val,0,15,min_sleep,max_sleep)
+calculate_rgb(hues,rgb_colors,brightness)
+
+new_strip_colors(rgb_colors,led_colors)
 for i in range(numpix):
     strip[i]=led_colors[i]
 strip.write()
 
-new_strip_hues(led_colors)
+new_strip_colors(rgb_colors,led_colors)
     
 led_num=0
 
 segment_len=numpix/2
 
+
+pot_check=5
+
 while True:
+    pot_check-=1
+    if pot_check<=0:
+        pot_check=5
+        pot_val=pot.value>>12
+#        pot_val=pot.value
+        
+        if pot_val!=last_pot_val:
+            brightness=int(map(pot_val,0,15,128,255))
+            sleep_time=map(15-pot_val,0,15,min_sleep,max_sleep)
+            calculate_rgb(hues,rgb_colors,brightness)
+            last_pot_val=pot_val
+    
     while led_num < segment_len and strip[led_num]==tuple(led_colors[led_num]):
         led_num+=1
 
@@ -78,7 +119,7 @@ while True:
         led_num+=1
 
     if led_num >= segment_len:
-        new_strip_hues(led_colors)
+        new_strip_colors(rgb_colors,led_colors)
         led_num=0
 
-    time.sleep(0.08)
+    time.sleep(sleep_time)
